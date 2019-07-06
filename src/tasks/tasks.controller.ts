88b3c653ad79fd1b23b070271912614371825b69
Shubham Jain <shubham.jain@engineerbabu.in>
2019-07-06 12:42:56 +0530

    get task by Id & delete task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './tasks.model';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -13,8 +13,18 @@ export class TasksController {
         return this.tasksService.getAllTasks();
     }
 
+    @Get('/:id')
+    getTaskById(@Param('id') id: string): Task {
+        return this.tasksService.getTaskById(id);
+    }
+
     @Post()
     createTask(@Body() taskDto: CreateTaskDto): Task {
         return this.tasksService.createTask(taskDto);
+    }
+
+    @Delete('/:id')
+    deleteTask(@Param('id') id: string): void {
+        this.tasksService.deleteTask(id);
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -11,6 +11,10 @@ export class TasksService {
         return [...this.tasks];
     }
 
+    getTaskById(id: string): Task {
+        return { ...this.tasks.find(task => task.id === id) };
+    }
+
     createTask(taskDto: CreateTaskDto): Task {
         const { title, description } = taskDto;
         const task: Task = {
@@ -22,5 +26,9 @@ export class TasksService {
 
         this.tasks.push(task);
         return task;
+    }
+
+    deleteTask(id: string): void {
+        this.tasks = this.tasks.filter(task => task.id !== id);
     }
 }
