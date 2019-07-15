c40d4bd2cbecb1f76aa6b94e4e4aa330b7523e2c
Shubham Jain <shubham.jain@engineerbabu.in>
2019-07-15 19:54:12 +0530

    handling unique username constraint while signup

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,7 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredientialsDto } from './dto/auth-credentials.dto';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -11,8 +12,15 @@ export class UserRepository extends Repository<User> {
         const user = new User();
         user.username = username;
         user.password = password;
-        await user.save();
-        // return user;
+        try {
+            await user.save();
+        } catch (err) {
+            if (err.code === '23505') {
+                throw new ConflictException('Username already exists!');
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
     }
 
 }
