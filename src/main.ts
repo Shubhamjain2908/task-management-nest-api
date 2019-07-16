fa70ead96d600952e7fc700f5e700e6593ad8cd5
Shubham Jain <shubham.jain@engineerbabu.in>
2019-07-16 23:43:35 +0530

    enabling cors

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,11 @@ async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
   app.use(morgan('dev'));
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
